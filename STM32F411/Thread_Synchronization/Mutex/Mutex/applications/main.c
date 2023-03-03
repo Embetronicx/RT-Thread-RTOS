22031e7c82be8e5586c9355f0080395311a1c7b1
EmberTronicX <embetronicx@gmail.com>
2023-03-03 22:36:41 +0530

    added event and and mutex example

---
+++ STM32F411/Thread_Synchronization/Mutex/Mutex/applications/main.c
@@ -0,0 +1,84 @@
+#include <rtthread.h>
+
+#define DBG_TAG "main"
+#define DBG_LVL DBG_LOG
+
+#include <rtdbg.h>
+#include <drv_common.h>
+#include <rtdevice.h>
+
+uint32_t global_var = 10;
+
+static rt_mutex_t mutex = RT_NULL;
+
+static rt_thread_t threadId1 = RT_NULL;
+static rt_thread_t threadId2 = RT_NULL;
+
+/* Thread 1 */
+static void thread1_function(void *parameter)
+{
+    while (1)
+    {
+        rt_mutex_take(mutex, RT_WAITING_FOREVER);
+
+        global_var++;                       //Accessing common resource
+        LOG_D("Thread 1: global_var = %d\n", global_var);
+
+        rt_mutex_release(mutex);
+
+        rt_thread_mdelay(1000);
+    }
+}
+
+/* Thread 2 */
+static void thread2_function(void *parameter)
+{
+    while (1)
+    {
+        rt_mutex_take(mutex, RT_WAITING_FOREVER);
+
+        global_var--;                       //Accessing common resource
+        LOG_D("Thread 2: global_var = %d\n", global_var);
+
+        rt_mutex_release(mutex);
+
+
+        rt_thread_mdelay(1000);
+    }
+}
+
+
+int main(void)
+{
+    /* Create a dynamic mutex */
+    mutex = rt_mutex_create("EtXmutex", RT_IPC_FLAG_FIFO);
+    if (mutex == RT_NULL)
+    {
+        LOG_D("create dynamic mutex failed.\n");
+        return -1;
+    }
+
+    /* Create thread 1 */
+    threadId1 = rt_thread_create("thread1_fn",  //Name
+                            thread1_function,   //Function address
+                            RT_NULL,            //Thread function parameter
+                            1024,               //Stack Size
+                            1,                  //Thread priority
+                            2000);              //Time slice in ticks
+
+    /* Create thread 2 */
+    threadId2 = rt_thread_create("thread2_fn",  //Name
+                            thread2_function,   //Function address
+                            RT_NULL,            //Thread function parameter
+                            1024,               //Stack Size
+                            0,                  //Thread priority
+                            2000);              //Time slice in ticks
+
+    /* Start both threads */
+    rt_thread_startup(threadId1);
+    rt_thread_startup(threadId2);
+
+    while(1);
+
+    return RT_EOK;
+}
